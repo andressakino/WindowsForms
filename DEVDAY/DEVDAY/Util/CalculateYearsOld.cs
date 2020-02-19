2b97bd9125ce6db91ea70e3b756fae48e7659e13
Paulo Santos <paulo.santos@jjconsulting.com.br>
2020-02-19 16:28:33 -0300

    Ajuste de conflito

---
+++ DEVDAY/DEVDAY/Util/CalculateYearsOld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DEVDAY.Util
+{
+    public class CalculateYearsOld
+    {
+        public int GetYearsOld(DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+
+            var a = (today.Year * 100 + today.Month) * 100 + today.Day;
+            var b = (dateOfBirth.Year * 100 + dateOfBirth.Month) * 100 + dateOfBirth.Day;
+
+            return (a - b) / 10000;
+        }
+    }
+}
